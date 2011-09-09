30b5d03e0288a61d72994dd5de74db6833e5d3cb
George Tsiokos <hitec@tsiokos.com>
2011-09-09 11:29:48 -0400

    Deferred null argument exception for AsAsyncResult, enhanced ToKeyedSubject to support custom subjects

--- corlib/ObservableExtensions.cs
+++ corlib/ObservableExtensions.cs
@@ -14,10 +14,13 @@ namespace CorLib {
         /// <param name="state">APM state</param>
         /// <returns>an async result representing the single value from the sequence</returns>
         /// <remarks>if the sequence errors, returns a value, or completes empty, the async result signals</remarks>
-        /// <exception cref="ArgumentNullException">thrown when sequence is null</exception>
         public static IAsyncResult<T> AsAsyncResult<T> (this IObservable<T> sequence, AsyncCallback asyncCallback, object state) {
             if (sequence == null)
-                throw new ArgumentNullException ("sequence", "sequence is null.");
+                return AsyncResult.CreateCompleted<T> (
+                    asyncCallback,
+                    state,
+                    new ArgumentNullException ("sequence", "sequence is null."));
+
             var result = AsyncResult.Create<T> (asyncCallback, state);
             // subscription will be completed when the sequence produces a single value, errors, or completes
             sequence.Take (1).Subscribe (result);
