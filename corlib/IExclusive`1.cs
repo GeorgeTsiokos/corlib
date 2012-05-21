86dfbea5a5737c0f4f98e52a3c0fc7dc30728f89
George Tsiokos <hitec@tsiokos.com>
2012-05-21 15:46:17 -0400

    IExclusive<T> commit!

---
+++ corlib/IExclusive`1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Corlib {
+    /// <summary>
+    /// Encapsulates a value for exclusive access
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="IDisposable.Dispose"/> cancels any 
+    /// outstanding scheduled actions and frees resources
+    /// healed by the object. To create a value for exclusive access,
+    /// 
+    /// </remarks>
+    public interface IExclusive<T> : IDisposable {
+        /// <summary>
+        /// Schedules an action to take place with exclusive access to the value
+        /// </summary>
+        IDisposable Schedule (Action<T> value);
+    }
+}
